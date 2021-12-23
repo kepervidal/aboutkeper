bbc8f59238a0603d449cf8921b493e632698538d
kepervidal <96431603+kepervidal@users.noreply.github.com>
2021-12-23 16:51:40 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,6 @@
+var quantidade = 10;
+var preco = 6.4;
+let imposto = 1.5;
+
+console.log(preco)
+console.log(quantidade)
---
+++ dados_2.js
@@ -0,0 +1,4 @@
+let preco = 19.90;
+let desconto = 0.4;
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
